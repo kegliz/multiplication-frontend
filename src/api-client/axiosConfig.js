aea82b877a50df3b61e7553e3013d8557306f735
Zoltan Kegli <kegli.zoltan@gmail.com>
2021-06-21 14:22:22 +0200

    using .env

--- src/api-client/axiosConfig.js
+++ src/api-client/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 //export const BASE_URL = "https://navu.shadday.tech";
 //export const BASE_URL = "http://localhost:8080";
-const { SNOWPACK_PUBLIC_BASE_URL } = __SNOWPACK_ENV__;
+//const { SNOWPACK_PUBLIC_BASE_URL } = __SNOWPACK_ENV__;
 
 const instance = axios.create({
   baseURL: import.meta.env.SNOWPACK_PUBLIC_BASE_URL,
